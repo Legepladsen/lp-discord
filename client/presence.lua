69dbb0979c51bdcd8c15c4ef7dcbf1fd270ffca9
rallemakralle <livligere-dumpet-5w@icloud.com>
2026-03-04 19:41:19 +0100

    Made an object for passing through Presence updates with some little documentation

--- client/presence.lua
+++ client/presence.lua
@@ -1,16 +1,9 @@
-local Presence = {
-    Activity: string,
-    AssetText: string,
-    SmallAssetText: string,
-}
-
 AddEventHandler('onResourceStart', function (resourceName)
     if (GetCurrentResourceName() ~= resourceName) then
         return
     end
     if Config.applicationId == nil or Config.applicationId == '' then
         print('[lp-discord] Error: No applicationId set, did you forget to restart the resource after editing?')
-        CancelEvent()
         return
     else
         initialSetup()
@@ -18,17 +11,27 @@ AddEventHandler('onResourceStart', function (resourceName)
     end
 end)
 
-local function initialSetup () {
+local function initialSetup () 
     SetDiscordAppId(Config.applicationId)
     SetRichPresence(Config.richPresence)
     SetDiscordRichPresenceAsset(Config.discordRichPresenceAsset)
     SetDiscordRichPresenceAssetText(Config.discordRichPresenceAssetText)
     SetDiscordRichPresenceAssetSmall(Config.discordRichPresenceAssetSmall)
     SetDiscordRichPresenceAssetSmallText(Config.discordRichPresenceAssetSmallText)
-}
+end
 
-function UpdatePresence(presence:Presence) {
+---@class Presence The objects that must be passed as an object when wanting to update the users presence
+---@field Activity string
+---@field AssetText string
+---@field AssetSmallText string
+
+---@param presence Presence
+function UpdatePresence(presence)
     SetRichPresence(presence.Activity)
     SetDiscordRichPresenceAssetText(presence.AssetText)
-    SetDiscordRichPresenceAssetSmalltext(presence.SmallAssetText)
-}
+    SetDiscordRichPresenceAssetSmallText(presence.AssetSmallText)
+end
+
+RegisterNUICallBack('updateUserPresence', function(data, cb)
+    
+end)
